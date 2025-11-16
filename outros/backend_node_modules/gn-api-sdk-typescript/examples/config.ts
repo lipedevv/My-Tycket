db59f72a784cabe736b5f260818ab3e01c6c6c17
K4duDEV <bahkadu@gmail.com>
2025-11-16 17:55:54 -0300

    Update

---
+++ outros/backend_node_modules/gn-api-sdk-typescript/examples/config.ts
@@ -0,0 +1,8 @@
+export = {
+    // PRODUÇÃO = false
+    // HOMOLOGAÇÃO = true
+    sandbox: false,
+    client_id: 'seuClientId',
+    client_secret: 'seuClientSecret',
+    pix_cert: 'caminhoAteOCertificadoPix',
+};
